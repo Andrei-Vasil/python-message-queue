7cc2f68a249a4446796407c82dc125a4a8fa0fb2
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-08 12:08:54 +0300

    fully mapped async pub/sub system api

--- async/httpRequestHandler.py
+++ async/httpRequestHandler.py
@@ -1,5 +1,4 @@
 from quart import Quart, request
-from lib.queue import AsyncQueue
 from subscriptionManager import SubscriptionManager
 from topicManager import TopicManager
 from queueManager import QueueManager
@@ -25,18 +24,38 @@ async def remove(topic):
         return f'Successfully deleted {topic} topic', 200
     except Exception as e:
         return str(e), 404
-
-# TODO: map each request url
+    
+@httpRequestHandler.route("/subscription/<topic>", methods=['POST'])
+async def subscribe(topic):
+    try:
+        id = await subscriptionManager.subscribe(topic)
+        return f'{id}', 200
+    except Exception as e:
+        return str(e), 404
+    
+@httpRequestHandler.route("/subscription/<topic>/<id>", methods=['DELETE'])
+async def unsubscribe(topic, id):
+    try:
+        await subscriptionManager.unsubscribe(topic, id)
+        return f'Successfully unsubscribed id {id} from {topic} topic', 200
+    except Exception as e:
+        return str(e), 404
+    
+@httpRequestHandler.route("/publish/<topic>", methods=['POST'])
+async def publish(topic):
+    try:
+        form = await request.json
+        await queueManager.publishMessage(topic, str(form['item']))
+        return f'Successfully published your message to {topic} topic', 200
+    except Exception as e:
+        return str(e), 404
+    
+@httpRequestHandler.route("/subscription/<topic>/<id>", methods=['GET'])
+async def retrieve(topic, id):
+    try:
+        message = await queueManager.retrieveMessage(topic, id)
+        return f'{message}', 200
+    except Exception as e:
+        return str(e), 404
 
 httpRequestHandler.run()
-
-
-# @httpRequestHandler.route("/push", methods=['POST'])
-# async def push():
-#     form = await request.json
-#     await queue.push(form['item'])
-#     return ''
-
-# @httpRequestHandler.route("/pop", methods=['GET'])
-# async def pop():
-#     return str(await queue.pop())
