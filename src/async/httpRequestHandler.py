456d169f74d8611d18c66ca416993edfa984fe87
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-19 16:55:53 +0300

    change pub/sub messages from int to string

--- src/async/httpRequestHandler.py
+++ src/async/httpRequestHandler.py
@@ -13,50 +13,50 @@ async def new(topic):
     try:
         await topicManager.new(topic)
         await queueManager.topicAdded(topic)
-        return f'Successfully created the new topic {topic}', 200
+        return f'Successfully created the new topic {topic}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
 
 @httpRequestHandler.route("/topic/<topic>", methods=['DELETE'])
 async def remove(topic):
     try:
         await topicManager.remove(topic)
         await queueManager.topicRemoved(topic)
-        return f'Successfully deleted {topic} topic', 200
+        return f'Successfully deleted {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>", methods=['POST'])
 async def subscribe(topic):
     try:
         id = await subscriptionManager.subscribe(topic)
-        return f'{id}', 200
+        return f'{id}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>/<id>", methods=['DELETE'])
 async def unsubscribe(topic, id):
     try:
         await subscriptionManager.unsubscribe(topic, id)
-        return f'Successfully unsubscribed id {id} from {topic} topic', 200
+        return f'Successfully unsubscribed id {id} from {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/publish/<topic>", methods=['POST'])
 async def publish(topic):
     try:
         form = await request.json
         await queueManager.publishMessage(topic, str(form['item']))
-        return f'Successfully published your message to {topic} topic', 200
+        return f'Successfully published your message to {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>/<id>", methods=['GET'])
 async def retrieve(topic, id):
     try:
         message = await queueManager.retrieveMessage(topic, id)
-        return f'{message}', 200
+        return f'{message}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
 
 httpRequestHandler.run()
--- src/multi_threaded/httpRequestHandler.py
+++ src/multi_threaded/httpRequestHandler.py
@@ -14,51 +14,51 @@ def new(topic):
     try:
         topicManager.new(topic)
         queueManager.topicAdded(topic)
-        return f'Successfully created the new topic {topic}', 200
+        return f'Successfully created the new topic {topic}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
 
 @httpRequestHandler.route("/topic/<topic>", methods=['DELETE'])
 def remove(topic):
     try:
         topicManager.remove(topic)
         queueManager.topicRemoved(topic)
-        return f'Successfully deleted {topic} topic', 200
+        return f'Successfully deleted {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>", methods=['POST'])
 def subscribe(topic):
     try:
         id = subscriptionManager.subscribe(topic)
-        return f'{id}', 200
+        return f'{id}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>/<id>", methods=['DELETE'])
 def unsubscribe(topic, id):
     try:
         subscriptionManager.unsubscribe(topic, id)
-        return f'Successfully unsubscribed id {id} from {topic} topic', 200
+        return f'Successfully unsubscribed id {id} from {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/publish/<topic>", methods=['POST'])
 def publish(topic):
     try:
         form = request.json
         queueManager.publishMessage(topic, str(form['item']))
-        return f'Successfully published your message to {topic} topic', 200
+        return f'Successfully published your message to {topic} topic\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
     
 @httpRequestHandler.route("/subscription/<topic>/<id>", methods=['GET'])
 def retrieve(topic, id):
     try:
         message = queueManager.retrieveMessage(topic, id)
-        return f'{message}', 200
+        return f'{message}\r\n', 200
     except Exception as e:
-        return str(e), 404
+        return f'{str(e)}\r\n', 404
 
 with MultiThreadedQueue() as queue:
     httpRequestHandler.run()
