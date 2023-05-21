316b0f72e30d6b3f0a5ce97d2adc4360ca2df27b
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-21 21:18:51 +0300

    renamed const variables

--- src/benchmark/benchmark.py
+++ src/benchmark/benchmark.py
@@ -3,12 +3,11 @@ import os
 import time
 
 BENCHMARKING = True
-
-benchmark_id = 2
-benchmark_file = f'data/{datetime.date.today()}-{benchmark_id}.csv'
+BENCHMARK_ID = 2
+BENCHMARK_FILE = f'data/{datetime.date.today()}-{BENCHMARK_ID}.csv'
 
 def mark_end_of_push(benchmark_id: str):
     if not BENCHMARKING:
         return
-    os.system(f'echo {time.time()},{benchmark_id} >> {benchmark_file}')
+    os.system(f'echo {time.time()},{benchmark_id} >> {BENCHMARK_FILE}')
 
