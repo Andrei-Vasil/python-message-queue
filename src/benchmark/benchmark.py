a477535e2b716829e591e75378356e614c1c987f
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-05-22 11:57:25 +0300

    save seconds instead of nanoseconds for throughput

--- src/benchmark/benchmark.py
+++ src/benchmark/benchmark.py
@@ -3,7 +3,7 @@ import os
 import time
 
 BENCHMARKING = True
-BENCHMARK_ID = 7
+BENCHMARK_ID = 9
 LATENCY_BENCHMARK_FILE = f'data/latency/{datetime.date.today()}-{BENCHMARK_ID}.csv'
 PRODUCER_THROUGHPUT_BENCHMARK_FILE = f'data/throughput/producer/{datetime.date.today()}-{BENCHMARK_ID}.csv'
 CONSUMER_THROUGHPUT_BENCHMARK_FILE = f'data/throughput/consumer/{datetime.date.today()}-{BENCHMARK_ID}.csv'
@@ -16,10 +16,10 @@ def mark_end_of_push(benchmark_id: str):
 def count_producer_throughput():
     if not BENCHMARKING:
         return
-    os.system(f'echo {time.time()} >> {PRODUCER_THROUGHPUT_BENCHMARK_FILE}')
+    os.system(f'echo {int(time.time())} >> {PRODUCER_THROUGHPUT_BENCHMARK_FILE}')
 
 def count_consumer_throughput():
     if not BENCHMARKING:
         return
-    os.system(f'echo {time.time()} >> {CONSUMER_THROUGHPUT_BENCHMARK_FILE}')
+    os.system(f'echo {int(time.time())} >> {CONSUMER_THROUGHPUT_BENCHMARK_FILE}')
 
